5d79f3ae41730bc692b4c726832d5944f151e723
FacundoMina <facundomina095@gmail.com>
2026-02-19 09:26:49 -0300

    Agregar archivos de proyecto.

---
+++ ClassLibraryFinal/Data/AplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ClassLibraryFinal.Models;
+
+namespace ClassLibraryFinal.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public DbSet<Class1> Class1s { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=LAPTOP-B9HDS386;Database=SuerteDB;Trusted_Connection=True;TrustServerCertificate=True;"
+
+            );
+        }
+    }
+}
---
+++ ClassLibraryFinal/Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassLibraryFinal.Models
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+
+    }
+}
